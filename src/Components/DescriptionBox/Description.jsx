7089ba3985956cb8dd7911903b1f9535485fd148
jayapriyan-m <jayapriyan11802@gmail.com>
2024-02-12 11:54:12 +0530

    Ready to Deploy

---
+++ src/Components/DescriptionBox/Description.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const Description = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (175)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae odit rem at! Aperiam dicta minima consequatur similique qui dignissimos dolorem ut iure facere officia, cumque alias? Voluptate repudiandae adipisci nesciunt.
+            Accusamus, minima. Commodi rem autem quidem nam explicabo itaque, sint cum qui, rerum placeat molestias   </p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus soluta, harum molestiae nisi rem sed corrupti perspiciatis illum dolorem eum animi. Atque deserunt assumenda provident illum perspiciatis temporibus illo architecto.
+            Veritatis vero animi corrupti sapiente.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Description
