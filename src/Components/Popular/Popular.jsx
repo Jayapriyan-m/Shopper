8fc3326147b4d15324ee89fe120ef5b18fe0af16
jayapriyan-m <jayapriyan11802@gmail.com>
2024-02-12 15:15:45 +0530

    Added Render backend  link

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular =() => {
     const [popularProducts , setPopularProducts] = useState([]);
 
     useEffect(()=>{
-        fetch('https://shopper-4atx.onrender.com/popularinwomen')
+        fetch('https://shopper-api.onrender.com/popularinwomen')
         .then((response)=>response.json())
         .then((data=>setPopularProducts(data)))
     },[])
